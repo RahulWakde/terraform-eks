35a53a3c0bbf5c79d9a17f0f37d3da8a54d49536
RahulWakde <160769182+RahulWakde@users.noreply.github.com>
2026-01-29 23:20:17 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,11 +3,13 @@ module "vpc" {
 }
 
 module "eks" {
-  source       = "./modules/eks"
-  cluster_name = var.cluster_name
-  vpc_id       = module.vpc.vpc_id
-  subnet_ids   = module.vpc.public_subnets
+  source = "./modules/eks"
+
+  cluster_name  = "demo-eks"
+  subnets       = module.vpc.public_subnets
+  instance_type = "t3.medium"
 }
+
 
 data "aws_eks_cluster" "this" {
   name = module.eks.cluster_name
