563b38613629f8373d5722b1909c56cf62e554b3
RahulWakde <160769182+RahulWakde@users.noreply.github.com>
2026-01-30 00:28:18 +0530

    Update outputs.tf

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -1,2 +1,9 @@
-output "vpc_id" { value = aws_vpc.main.id }
-output "private_subnets" { value = aws_subnet.private[*].id }
+output "vpc_id" {
+  description = "The ID of the VPC"
+  value       = aws_vpc.main.id
+}
+
+output "private_subnets" {
+  description = "List of IDs of private subnets"
+  value       = aws_subnet.private[*].id
+}
