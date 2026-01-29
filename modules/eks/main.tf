992e201bae4537fa9a3162ec1760a7c85ba167fd
RahulWakde <160769182+RahulWakde@users.noreply.github.com>
2026-01-29 23:18:06 +0530

    Update main.tf

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -21,8 +21,10 @@ resource "aws_eks_cluster" "this" {
   role_arn = aws_iam_role.eks_cluster.arn
 
   vpc_config {
-    subnet_ids = var.subnet_ids
+    subnet_ids = var.subnets
   }
+}
+
 
   depends_on = [aws_iam_role_policy_attachment.eks_cluster_policy]
 }
@@ -47,13 +49,16 @@ resource "aws_iam_role_policy_attachment" "node_policy" {
 
 resource "aws_eks_node_group" "this" {
   cluster_name    = aws_eks_cluster.this.name
-  node_group_name = "demo-ng"
-  node_role_arn  = aws_iam_role.node.arn
-  subnet_ids     = var.subnet_ids
+  node_group_name = "${var.cluster_name}-ng"
+  node_role_arn  = aws_iam_role.eks_node.arn
+  subnet_ids     = var.subnets
 
   scaling_config {
     desired_size = 2
     max_size     = 3
     min_size     = 1
   }
+
+  instance_types = [var.instance_type]
 }
+
