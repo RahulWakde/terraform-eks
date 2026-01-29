89fb105c56bf303b9fd22ba291efe4e595e182e2
RahulWakde <160769182+RahulWakde@users.noreply.github.com>
2026-01-30 00:27:39 +0530

    Update outputs.tf

--- modules/eks/outputs.tf
+++ modules/eks/outputs.tf
@@ -1,11 +1,14 @@
+output "cluster_name" {
+  description = "The name of the EKS cluster"
+  value       = aws_eks_cluster.this.name
+}
+
 output "cluster_endpoint" {
-  value = aws_eks_cluster.this.endpoint
+  description = "The endpoint for the EKS Kubernetes API"
+  value       = aws_eks_cluster.this.endpoint
 }
 
 output "cluster_ca" {
-  value = aws_eks_cluster.this.certificate_authority[0].data
-}
-
-output "cluster_name" {
-  value = aws_eks_cluster.this.name
+  description = "The base64 encoded certificate data required to communicate with the cluster"
+  value       = aws_eks_cluster.this.certificate_authority[0].data
 }
