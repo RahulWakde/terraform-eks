ee1995851cb5d7c9d537017b0a5c615dc9b35ded
Ubuntu <ubuntu@ip-172-31-47-52.ap-south-1.compute.internal>
2026-01-29 18:45:36 +0000

    remove backend

--- envs/dev/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "terraform-state-eks-mahesh-2026"
-    key            = "dev/eks.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "terraform-lock"
-  }
-}
