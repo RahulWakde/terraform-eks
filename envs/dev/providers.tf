b584068366f31246e72fc5addc593f5d16d7c51f
RahulWakde <160769182+RahulWakde@users.noreply.github.com>
2026-01-30 00:25:26 +0530

    Update providers.tf

--- envs/dev/providers.tf
+++ envs/dev/providers.tf
@@ -1,10 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.0"
+    }
+  }
+}
+
 provider "aws" {
   region = var.region
 }
 
 provider "helm" {
-  # Note: Ensure there is NO equals sign after 'kubernetes'
-  # but the internal fields must be correct.
   kubernetes {
     host                   = module.eks.cluster_endpoint
     cluster_ca_certificate = base64decode(module.eks.cluster_ca)
