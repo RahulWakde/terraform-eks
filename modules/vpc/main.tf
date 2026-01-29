a90cd5b3c79fd26879979802407e03932f381c4d
RahulWakde <160769182+RahulWakde@users.noreply.github.com>
2026-01-29 22:50:30 +0530

    Update main.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,21 +1,13 @@
 resource "aws_vpc" "this" {
-  cidr_block           = var.vpc_cidr
-  enable_dns_support   = true
-  enable_dns_hostnames = true
-
-  tags = {
-    Name = "eks-vpc"
-  }
+  cidr_block = "10.0.0.0/16"
 }
 
 resource "aws_subnet" "public" {
-  count                   = length(var.public_subnets)
+  count                   = 2
   vpc_id                  = aws_vpc.this.id
-  cidr_block              = var.public_subnets[count.index]
-  availability_zone       = var.azs[count.index]
+  cidr_block              = cidrsubnet("10.0.0.0/16", 8, count.index)
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
   map_public_ip_on_launch = true
-
-  tags = {
-    Name = "public-subnet-${count.index}"
-  }
 }
+
+data "aws_availability_zones" "available" {}
