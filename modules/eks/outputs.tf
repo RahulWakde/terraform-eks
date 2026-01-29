719008da79e035e54a537f67c31b0242e4dcb18e
RahulWakde <160769182+RahulWakde@users.noreply.github.com>
2026-01-29 22:52:23 +0530

    Update outputs.tf

--- modules/eks/outputs.tf
+++ modules/eks/outputs.tf
@@ -1,14 +1,3 @@
-output "endpoint" {
-  value = aws_eks_cluster.this.endpoint
-}
-
-output "ca" {
-  value = aws_eks_cluster.this.certificate_authority[0].data
-}
-
-output "token" {
-  value = data.aws_eks_cluster_auth.this.token
-}
-data "aws_eks_cluster_auth" "this" {
-  name = aws_eks_cluster.this.name
+output "cluster_name" {
+  value = aws_eks_cluster.this.name
 }
