e08b23195662559811c9344345ce3d390edcf97d
RahulWakde <160769182+RahulWakde@users.noreply.github.com>
2026-01-29 23:54:38 +0530

    Update main.tf

--- modules/nginx/main.tf
+++ modules/nginx/main.tf
@@ -1,61 +1,11 @@
-resource "kubernetes_namespace" "nginx" {
-  metadata {
-    name = "nginx"
-  }
-}
+resource "helm_release" "nginx" {
+  name       = "ingress-nginx"
+  repository = "https://kubernetes.github.io/ingress-nginx"
+  chart      = "ingress-nginx"
+  namespace  = "kube-system"
 
-resource "kubernetes_deployment" "nginx" {
-  metadata {
-    name      = "nginx"
-    namespace = kubernetes_namespace.nginx.metadata[0].name
-  }
-
-  spec {
-    replicas = 2
-
-    selector {
-      match_labels = {
-        app = "nginx"
-      }
-    }
-
-    template {
-      metadata {
-        labels = {
-          app = "nginx"
-        }
-      }
-
-      spec {
-        container {
-          name  = "nginx"
-          image = "nginx:latest"
-
-          port {
-            container_port = 80
-          }
-        }
-      }
-    }
-  }
-}
-
-resource "kubernetes_service" "nginx" {
-  metadata {
-    name      = "nginx"
-    namespace = kubernetes_namespace.nginx.metadata[0].name
-  }
-
-  spec {
-    selector = {
-      app = "nginx"
-    }
-
-    port {
-      port        = 80
-      target_port = 80
-    }
-
-    type = "LoadBalancer"
+  set {
+    name  = "controller.service.type"
+    value = "LoadBalancer"
   }
 }
